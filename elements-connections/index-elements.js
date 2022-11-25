c3bb8af82d60acc6f376fa4855d5dc158dbee951
Leandro_SmS <leandrosimoesms@gmail.com>
2022-11-25 17:54:10 -0300

    added all circles and lines to FOR loop to group

--- elements-connections/index-elements.js
+++ elements-connections/index-elements.js
@@ -44,11 +44,15 @@ const visSvg = d3.select("#svgGraph")
 //for each element of matutu, creat a line for each output
 for (let elem of elements) {
       let outputsLines = elem.elementOutputs(elementsRelations);
-      visSvg.selectAll('g')
+      console.log(outputsLines)
+      
+      let g = visSvg.selectAll('g')
             //returns a array with outputsX1Y1, outputsx2Y2 and the thing they are exchanging
             .data(outputsLines)
             .enter()
-            .append('line')
+            .append('g');
+
+      g.append('line')
             .attr('x1', (d) => d.outputsX1Y1[0])
             .attr('y1', (d) => h - d.outputsX1Y1[1])
             .attr('x2', (d) => d.outputsX2Y2[0])
@@ -57,20 +61,23 @@ for (let elem of elements) {
             .style("stroke-width", 5)
             .append('title')
             .text((d) => d.output);
-}
 
-const node = visSvg.selectAll('circle')
-      .data(elements)
-      .enter()
-      .append('g')
-      .append('circle')
-      .attr('r', 20)
-      .attr('cx', (d) => d.randomXY[0])
-      .attr('cy', (d) => h - d.randomXY[1]) // y is always inverted
-      .style("fill", "Tomato");
+      const outputCircles = g
+            .append('circle')
+            .attr('r', 20)
+            .attr('cx', (d) => d.outputsX1Y1[0])
+            .attr('cy', (d) => h - d.outputsX1Y1[1]) // y is always inverted
+            .style("fill", "Tomato");
 
-const g = visSvg.selectAll('g')
-      .on('mouseover', function (d, i) {
+      const inputCircles = g
+            .append('circle')
+            .attr('r', 20)
+            .attr('cx', (d) => d.outputsX2Y2[0])
+            .attr('cy', (d) => h - d.outputsX2Y2[1]) // y is always inverted
+            .style("fill", "Tomato");
+
+      
+      g.on('mouseover', function (d, i) {
             d3.select(this).transition()
                 .duration('500')
                 .attr('opacity', '.55');
@@ -79,13 +86,21 @@ const g = visSvg.selectAll('g')
             d3.select(this).transition()
                 .duration('500')
                 .attr('opacity', '1');
-            })
-      .append('title')
-      .text((d) => {
-            return d.name;
-      });
+            });
+}
 
-const circlesTextLegend = visSvg.selectAll('g')
+/*const node = visSvg.selectAll('g').selectAll('circle')
+      .data(elements)
+      .enter()
+      .append('circle')
+      .attr('r', 20)
+      .attr('cx', (d) => d.randomXY[0])
+      .attr('cy', (d) => h - d.randomXY[1]) // y is always inverted
+      .style("fill", "Tomato");*/
+
+const circlesTextLegend = visSvg.selectAll('text')
+      .data(elements)
+      .enter()
       .append('text')
       .text((d) => d.name)
       .attr('class', 'textLegend')
@@ -102,4 +117,3 @@ const circlesTextLegend = visSvg.selectAll('g')
                 .duration('500')
                 .attr('opacity', '1');
             });
-
