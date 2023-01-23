f3158c52542940f2524a9c393e92602b9a7752e7
Leandro_SmS <leandrosimoesms@gmail.com>
2023-01-23 05:51:05 -0300

    Jquery to change cards

--- form/index-form.js
+++ form/index-form.js
@@ -5,6 +5,7 @@ $(document).ready(() => {
     
     $('#next-div-one').on('click', () => {
         $('#card-one').css('visibility', 'hidden');
+        $('#card-two').css('visibility', 'visible');
     });
 
 
