936c0af5c4664c5afaf05289685d418bc205a302
Leandro_SmS <leandrosimoesms@gmail.com>
2023-01-23 05:49:15 -0300

    added id to next-btn, removed prev btn from first form-div

--- form/index-form.js
+++ form/index-form.js
@@ -3,4 +3,9 @@ import { matutu } from "../class.js";
 
 $(document).ready(() => {
     
+    $('#next-div-one').on('click', () => {
+        $('#card-one').css('visibility', 'hidden');
+    });
+
+
 })
