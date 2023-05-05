7966f0b1059fce74814de216337afcfcc4c5a56a
Leandro_SmS <leandrosimoesms@gmail.com>
2023-05-04 22:04:20 -0300

    Set card styles

--- elements-connections-react/main.js
+++ elements-connections-react/main.js
@@ -1,31 +1,14 @@
 import { matutu } from "../elements/elements.js";
 
-const r = 200, w=800, h=500, cx=(w/2), cy=(h/2);
+const w = 450, h = 300, r = w > h ? w/4 : h/4, cx=(w/2), cy=(h/2);
+
 const elements = matutu.elements;
+
 matutu.positionElementsInCircle((w/2), (h/2), r, matutu.elements);
+
 const linesData = matutu.getRelationships().relationshipsLines;
 
-function Sitio() {
-    return (
-        <div className="element">
-            <h1>{matutu.name}</h1>
-            <p>{matutu.howAmI()}</p>
-            <div>
-                <h3>My elements are: </h3>
-                <ul>
-                    {elements.map((elem, i) => <li key={i}>{elem.name}</li>)}
-                </ul>
-            </div>
-        </div>
-    )
-}
-
 const dotsPosition = elements.map(elem => elem.circlePosition);
-console.log(dotsPosition)
-
-
-const offset = 20, text_offset = 10;
-
 
 function calculateTextPositions(centerX, centerY, radius, points) {
 
@@ -38,40 +21,60 @@ function calculateTextPositions(centerX, centerY, radius, points) {
     const textPositions = angles.map((angle, i) => {
       const x = centerX + radius * Math.cos(angle);
       const y = centerY + radius * Math.sin(angle);
-      return [x + (x - centerX) * 0.05, y + (y - centerY) * 0.1];
+      return [x + (x - centerX) * 0.05, y + (y - centerY) * 0.05];
     });
     
     return textPositions
   }
 
-  function textCorrection (centerX, centerY, points) {
-    const correction = points.map(point => {
-        let x = point[0];
-        let y = point[1];
-        if (x < centerX && y > centerY) {
-            point[0] = x - 35;
-            point[1] = y + 5;
-        }
-        if (x < centerX && y < centerY) {
-            point[0] = x - 25;
-            point[1] = y - 5;
-        }
-        return point;
-    })
-    return correction;
-  }
-  
-  // Call the function to get the text positions
-  const textPositions = textCorrection(cx, cy, calculateTextPositions(cx, cy, r, dotsPosition));
-  
-  // Output the text positions
-  console.log(textPositions);
+function textCorrection (centerX, centerY, points) {
+  const correction = points.map(point => {
+      let x = point[0];
+      let y = point[1];
+      if (x < centerX && y > centerY) {
+          point[0] = x - 35;
+          point[1] = y + 15;
+      }
+      if (x < centerX && y < centerY) {
+          point[0] = x - 25;
+          point[1] = y - 5;
+      }
+      if (x >= centerX && y < centerY) {
+        point[0] = x + 5;
+        point[1] = y - 5;
+    }
+    if (x >= centerX && y >= centerY) {
+        point[0] = x + 5;
+        point[1] = y + 5;
+    }
+      return point;
+  })
+  return correction;
+}
 
+// Call the function to get the text positions
+const textPositions = textCorrection(cx, cy, calculateTextPositions(cx, cy, r, dotsPosition));
+
+function Sitio() {
+    return (
+        <div className="element--title--info">
+            <h1>{matutu.name}</h1>
+            <p><span>Inputs:</span> {matutu.inputs.join(" / ")}</p>
+            <p><span>Outputs:</span> {matutu.outputs.join(" / ")}</p>
+            {/*<div className="elements--list">
+                <h3>Elements: </h3>
+                <ul>
+                    {elements.map((elem, i) => <li key={i}>{elem.name}</li>)}
+                </ul>
+            </div>*/}
+        </div>
+    )
+}
 
 function Lines(props) {
     return (
         <React.Fragment>
-            {textPositions.map((elem, i) =>  <text key={i} x={elem[0]} y={elem[1]}>{elements[i].name}</text>)}
+            {textPositions.map((elem, i) =>  <text fill="" key={i} x={elem[0]} y={elem[1]}>{elements[i].name}</text>)}
         </React.Fragment>
     )
 }
@@ -86,7 +89,7 @@ function Text() {
 
 function ElementsNetwork(props) {
     return (
-        <svg width={props.width} height={props.height}>
+        <svg width={props.width} height={props.height} className="svg">
             {elements.map((elem, i) => {
                 return (
                     <React.Fragment key={i}>
@@ -101,11 +104,44 @@ function ElementsNetwork(props) {
     )
 }
 
+function InputsCards () {
+    return (
+        <div className="elements--cards">
+                {elements.map((elem, i) => {
+                    return (
+                        <div key={i} className="card">
+                            <div className="face face1">
+                                <div className="content">
+                                    <h1>{elem.name}</h1>
+                                </div>
+                            </div>
+                            <div className="face face2">
+                                <div className="content">
+                                    <p>Inputs: {elem.inputs.join(" / ")}</p>
+                                    <p>Outputs: {elem.outputs.join(" / ")}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+    )
+}
+
+function ElementCard(props) {
+    return (
+        <div className="element">
+            <Sitio/>
+            <ElementsNetwork width={props.width} height={props.height}/>
+        </div>
+    )
+}
+
 function App() {
     return (
         <React.Fragment>
-            <Sitio/>
-            <ElementsNetwork width={800} height={500}/>
+            <ElementCard width={w} height={h} />
+            <InputsCards/>
         </React.Fragment>
     )
 }
