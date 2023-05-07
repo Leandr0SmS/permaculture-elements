2146913f65d791993d551f19046021ea0c51abf2
Leandro_SmS <leandrosimoesms@gmail.com>
2023-05-07 08:01:43 -0300

    Set dehydrator and crusher elements

--- elements/elements.js
+++ elements/elements.js
@@ -1,16 +1,17 @@
 import {Element} from "./class.js";
 
 //Sitio Matutu
-const matutu = new Element ("Sítio São José do Matutu", ['food', 'portion', 'gas', 'oil'], ['fertilizer', 'bananas', 'açaí'], []);
+const matutu = new Element ("Sítio São José do Matutu", ['food', 'portion', 'gas', 'oil', "fuel"], ['fertilizer', 'bananas', 'açaí'], []);
 
 //Elements in sitio Matutu
 const chicken = new Element ("Chicken", ["food", "water", "shelter", "portion"], ["food", "manure"], []);
 const house = new Element ("House", ["water", "eletricity", "gas", "food", "medicins"], ['shelter', "seeds", "sewage"], []);
 const garden = new Element ("Garden", ["water", "work", "seeds", "fertilizer"], ["food", "portion", "medicins"], []);
 const fosse = new Element ('Fosse', ['sewage'], [], []);
-const dehydrator = new Element ("Dehydrator", ["manure", "grass"], ["fertilizer"])
+const dehydrator = new Element ("Dehydrator", ["manure", "grass", "sawdust"], ["fertilizer"]);
+const crusher = new Element ("Crusher", ["oil", "fuel", "wood"], ["sawdust"])
 
 
-matutu.elements.push(chicken, house, garden, fosse, dehydrator);
+matutu.elements.push(chicken, house, garden, fosse, dehydrator, crusher);
 
 export {matutu};
