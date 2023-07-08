8c6f114e50a3b80be7fcdc6b61208409bb47ce25
Leandro_SmS <leandrosimoesms@gmail.com>
2023-07-08 06:49:51 -0300

    Set start and AC logic

---
+++ jucara-calculator/data/questions.js
@@ -0,0 +1,14 @@
+export const questions = [
+    {
+        id: 1,
+        question: "Quantos kilos de fruto?"
+    },
+    {
+        id: 2,
+        question: "Qual a porcentagem de água?"
+    },
+    {
+        id: 3,
+        question: "Qual a porcentagem de água?"
+    },
+]
