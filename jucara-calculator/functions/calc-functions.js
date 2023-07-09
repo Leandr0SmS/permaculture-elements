c68876a71c1c2d9b9bd38bfaa7c81c228b710cd7
Leandro_SmS <leandrosimoesms@gmail.com>
2023-07-09 07:29:18 -0300

    Refactor divide to handle low weights

--- jucara-calculator/functions/calc-functions.js
+++ jucara-calculator/functions/calc-functions.js
@@ -1,11 +1,17 @@
 const divide = (weigth) => {
     let divider = 1;
-    let operation = weigth;
-    while (operation > 9) {
-        operation = weigth / divider;
-        divider ++;
+    if (weigth / divider < 9) {
+        return {"rounds": divider, "weight": weigth};
+    } else {
+        let operation = weigth;
+        while (operation > 9) {
+            divider ++;
+            operation = weigth / divider;
+            console.log(divider)
+            console.log(operation)
+        }
+        return {"rounds": divider, "weight": parseFloat(operation.toFixed(2))};
     }
-    return {"rounds": divider, "weight": parseFloat(operation.toFixed(2))};
 }
 
-console.log(divide(20));
+export { divide };
