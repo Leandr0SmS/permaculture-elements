75f027aede93f6b053066c272790c9fa22e8a153
Leandro_SmS <leandrosimoesms@gmail.com>
2023-07-09 06:44:37 -0300

    Set divide function

--- jucara-calculator/functions/calc-functions.js
+++ jucara-calculator/functions/calc-functions.js
@@ -1,38 +1,11 @@
-const calculator = (prevNum, operator, nextNum) => {
-    switch (operator) {
-        case "x":
-            return prevNum * nextNum;
-        case "/":
-            return prevNum / nextNum;
-        case "+":
-            return prevNum + nextNum;
-        case "-":
-            return prevNum - nextNum;
-        default:
-            return "not valid operators"
+const divide = (weigth) => {
+    let divider = 1;
+    let operation = weigth;
+    while (operation > 9) {
+        operation = weigth / divider;
+        divider ++;
     }
-};
+    return {"rounds": divider, "weight": parseFloat(operation.toFixed(2))};
+}
 
-const operatorSelector = (opr, form) => {
-    for (let i = 0; i < form.length; i++) {
-        if (form[i] === opr) {
-          const calculatedValue = calculator(form[i - 1], form[i], form[i + 1]);
-          return [...form.slice(0, i -1), calculatedValue, ...form.slice(i + 2)];
-        }
-    }
-    return [...form]
-};
-
-const calculations = (formula) => {
-    if (formula.length <= 1) {
-        return formula
-    } else {
-        const mult = operatorSelector("x" || "/", formula);
-        const div = operatorSelector("/", mult);
-        const minus = operatorSelector("-", div);
-        const plus = operatorSelector("+" || "-", minus);
-        return calculations(plus);
-    }
-};
-
-export { calculations }
+console.log(divide(20));
